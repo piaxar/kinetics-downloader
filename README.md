115512d05c98e819810cc7ebc360f18e54982dda
Ivan Panchenko <piaxar@users.noreply.github.com>
2018-07-15 00:28:10 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # kinetics-downloader
+
+Simple tool to download videos from [kinetics dataset](https://deepmind.com/research/open-source/open-source-datasets/kinetics/)
+Also have functionality to trim downloaded videos to the length of action. To support trimming, ffmpeg should be installed and added to environment variable PATH.
+
+## Usage
+Install requirements first
+```
+pip install -r requirements.txt
+```
+
+You should download and unzip csv files with links to videos. You can download such files [here](https://deepmind.com/research/open-source/open-source-datasets/kinetics/).
+For example here is link to [kinetics-600 training.zip](https://deepmind.com/documents/193/kinetics_600_train%20(1).zip)
+
+### Downloading
+```
+cd kinetics-downloader
+python download.py /path/to/kinetic_train.csv /path/to/videos/
+```
+
+### Downloading and trimming
+```
+cd kinetics-downloader
+python download.py /path/to/kinetic_train.csv /path/to/videos/ --trim
+```
+
+### Parallel processing
+One can run downloading and trimming in several processes. To do so, just add flag --num-jobs with number of jobs running in parallel.
+For example:
+```
+cd kinetics-downloader
+python download.py /path/to/kinetic_train.csv /path/to/videos/ --trim --num-jobs 10
+```
---
+++ download.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import argparse
+import os
+import shutil
+import subprocess
+import pytube
+from joblib import delayed
+from joblib import Parallel
+
+REQUIRED_COLUMNS = ['label', 'youtube_id', 'time_start', 'time_end', 'split', 'is_cc']
+TRIM_FORMAT = '%06d'
+URL_BASE = 'https://www.youtube.com/watch?v='
+
+VIDEO_EXTENSION = '.mp4'
+VIDEO_FORMAT = 'mp4'
+
+
+def create_file_structure(path, folders_names):
+    """
+    Creates folders in specified path.
+    :return: dict
+        Mapping from label to absolute path folder, with videos of this label
+    """
+    mapping = {}
+    if not os.path.exists(path):
+        os.mkdir(path)
+    for name in folders_names:
+        dir_ = os.path.join(path, name)
+        if not os.path.exists(dir_):
+            os.mkdir(dir_)
+        mapping[name] = dir_
+    return mapping
+
+
+def download_clip(row, label_to_dir, trim):
+    """
+    Download clip from youtube.
+    row: dict-like objects with keys: ['label', 'youtube_id', 'time_start', 'time_end']
+    'time_start' and 'time_end' matter if trim is True
+    trim: bool, trim video to action ot not
+    """
+
+    label = row['label']
+    filename = row['youtube_id']
+    time_start = row['time_start']
+    time_end = row['time_end']
+
+    # if trim, save full video to tmp folder
+    output_path = label_to_dir['tmp'] if trim else label_to_dir[label]
+
+    # don't download if already exists
+    if not os.path.exists(os.path.join(output_path, filename + VIDEO_EXTENSION)):
+        print('Start downloading: ', filename)
+        pytube.YouTube(URL_BASE + filename).\
+            streams.filter(subtype=VIDEO_FORMAT).first().\
+            download(output_path, filename)
+        print('Finish downloading: ', filename)
+    else:
+        print('Already downloaded: ', filename)
+
+    if trim:
+        # Take video from tmp folder and put trimmed to final destination folder
+        # better write full path to video
+
+        start = str(time_start)
+        end = str(time_end - time_start)
+
+        input_filename = os.path.join(output_path, filename + VIDEO_EXTENSION)
+        output_filename = os.path.join(label_to_dir[label],
+                                       filename + '_{}_{}'.format(start, end) + VIDEO_EXTENSION)
+
+        if os.path.exists(output_filename):
+            print('Already trimmed: ', filename)
+        else:
+            print('Start trimming: ', filename)
+            # Construct command to trim the videos (ffmpeg required).
+            command = 'ffmpeg -i "{input_filename}" ' \
+                      '-ss {time_start} ' \
+                      '-t {time_end} ' \
+                      '-c:v libx264 -c:a copy -threads 1 ' \
+                      '"{output_filename}"'.format(
+                           input_filename=input_filename,
+                           time_start=start,
+                           time_end=end,
+                           output_filename=output_filename
+                       )
+            try:
+                subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError:
+                print('Error while trimming: ', filename)
+                return False
+            print('Finish trimming: ', filename)
+
+
+def main(input_csv, output_dir, trim, num_jobs):
+    assert input_csv[-4:] == '.csv', 'Provided input is not a .csv file'
+    links_df = pd.read_csv(input_csv)
+    assert all(elem in REQUIRED_COLUMNS for elem in links_df.columns.values),\
+        'Input csv doesn\'t contain required columns.'
+
+    # Creates folders where videos will be saved later
+    # Also create 'tmp' directory for temporary files
+    folders_names = links_df['label'].unique().tolist() + ['tmp']
+    label_to_dir = create_file_structure(path=output_dir,
+                                         folders_names=folders_names)
+
+    # Download files by links from dataframe
+    Parallel(n_jobs=num_jobs)(delayed(download_clip)(
+            row, label_to_dir, trim) for _, row in links_df.iterrows())
+
+    # Clean tmp directory
+    shutil.rmtree(label_to_dir['tmp'])
+
+
+if __name__ == '__main__':
+    description = 'Script for downloading and trimming videos from Kinetics dataset.' \
+                  'Supports Kinetics-400 as well as Kinetics-600.'
+    p = argparse.ArgumentParser(description=description)
+    p.add_argument('input_csv', type=str,
+                   help=('Path to csv file, containing links to youtube videos.\n'
+                         'Should contain following columns:\n'
+                         'label, youtube_id, time_start, time_end, split, is_cc'))
+    p.add_argument('output_dir', type=str,
+                   help='Output directory where videos will be saved.\n'
+                        'It will be created if doesn\'t exist')
+    p.add_argument('--trim', action='store_true', dest='trim', default=False,
+                   help='If specified, trims downloaded video, using values, provided in input_csv.\n'
+                        'Requires "ffmpeg" installed and added to environment PATH')
+    p.add_argument('--num-jobs', type=int, default=1,
+                   help='Number of parallel processes for downloading and trimming.')
+    main(**vars(p.parse_args()))
